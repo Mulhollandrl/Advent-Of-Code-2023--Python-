cf9a3f3f0b98f1894b568bcdc5dba30bf53aa934
Richard Mulholland <mulhollandrl@gmail.com>
2023-12-03 20:31:18 -0700

    Slightly buggy day1part2

--- Day 1/main1.py
+++ Day 1/main1.py
@@ -2,15 +2,20 @@ import re
 from enums import SPELLED_OUT_NUMBERS
 
 class Day1:
-    def __init__(self, file_path = "data/day1part1_input.csv") -> None:
+    def __init__(self, file_path = "c:/Users/Richard/csPersonal/AoC23 Py/Day 1/data/day1part1_input.csv") -> None:
         self.FILE_PATH = file_path
                 
     def get_first_last_digits(self, string, spelled=False):
         if spelled:
-            for word, num in SPELLED_OUT_NUMBERS.items():
-                string = string.replace(word, str(num))
+            # print(string, end="")
+            for i, character in enumerate(string):
+                for word, value in SPELLED_OUT_NUMBERS.items():
+                    if string[i:].startswith(word):
+                        string = string.replace(word, str(value), 1)
+            print(string, end="")
             
         numbers = [int(i) for i in re.findall(r'\d+', string)]
+        print(numbers)
         
         first = [int(digit) for digit in str(numbers[0])][0]
         last = [int(digit) for digit in str(numbers[-1])][-1]
@@ -31,13 +36,15 @@ class Day1:
         
         with open(self.FILE_PATH, 'r') as file:
             for line in file:
-                print(self.get_first_last_digits(line, True))
                 output += int(self.get_first_last_digits(line, True))
                 
         return output
 
 if __name__ == "__main__":
     day1 = Day1()
-    output = day1.day1part1()
+    output1 = day1.day1part1()
+    day1 = Day1()
+    output2 = day1.day1part2()
             
-    print(f"Your output Sire:\n{output}")
+    print(f"Your output1 Sire:\n{output1}")
+    print(f"Your output2 Sire:\n{output2}")
